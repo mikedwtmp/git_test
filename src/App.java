39a9f5ed6433e1ad7d4d41982c162dc8db9395b2
Mike Dreis <mikedreis@Mikes-MBP.attlocal.net>
2023-01-05 16:12:05 -0600

    added text

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Github Test");
-        System.out.println("Change to code.")
+        System.out.println("Change to code.");
+        System.out.println("branch added.");
     }
 }
